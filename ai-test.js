931a68a1a7bab2afe54bdbaef8c14b1447ec1354
xuess <wuniu2010@126.com>
2018-01-05 09:46:27 +0800

    添加官方音频样例

--- ai-test.js
+++ ai-test.js
@@ -3,8 +3,6 @@
  * xuess<wuniu2010@126.com>
  * 2017-12-24
  */
-
-
 var fs = require('fs');
 var config = require('./config.js');
 var AipSpeechClient = require("./src/index.js").speech;
@@ -15,20 +13,34 @@ var SECRET_KEY = config.SECRET_KEY;
 
 var client = new AipSpeechClient(APP_ID, API_KEY, SECRET_KEY);
 
-let voice1 = fs.readFileSync('./audio/amr.amr');
-let voiceBuffer1 = new Buffer(voice1);
 
-// 识别本地文件 amr 格式
-client.recognize(voiceBuffer1, 'amr', 16000).then(function(result) {
+
+let voice1 = fs.readFileSync('./audio/16k.pcm');
+let voiceBuffer1 = new Buffer(voice1);
+// 识别本地文件 pcm 格式
+client.recognize(voiceBuffer1, 'pcm', 16000).then(function(result) {
 	console.log('<recognize>: ' + JSON.stringify(result));
 }, function(err) {
 	console.log(err);
 });
 
-//文件稍微有点大 识别速度较慢，不推荐
-let voice3 = fs.readFileSync('./audio/wav.wav');
-let voiceBuffer3 = new Buffer(voice3);
 
+
+
+let voice2 = fs.readFileSync('./audio/16k-23850.amr');
+let voiceBuffer2 = new Buffer(voice2);
+// 识别本地文件 amr格式
+client.recognize(voiceBuffer2, 'amr', 16000).then(function(result) {
+	console.log('<recognize>: ' + JSON.stringify(result));
+}, function(err) {
+	console.log(err);
+});
+
+
+
+
+let voice3 = fs.readFileSync('./audio/16k.wav');
+let voiceBuffer3 = new Buffer(voice3);
 // 识别本地文件 wav格式
 client.recognize(voiceBuffer3, 'wav', 16000).then(function(result) {
 	console.log('<recognize>: ' + JSON.stringify(result));
